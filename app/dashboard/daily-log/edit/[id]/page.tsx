c646bedecfbea33395d7e70f616bc4a44aa9552b
Ubeyidah <ubeyidah@gmail.com>
2026-01-10 17:30:35 +0300

    feat: provide loading skeleton and reutrn not found if there is not found text in the message

--- app/dashboard/daily-log/edit/[id]/page.tsx
+++ app/dashboard/daily-log/edit/[id]/page.tsx
@@ -1,12 +1,10 @@
 import { Suspense } from "react";
-import { getQueryClient, trpc, caller } from "@/trpc/server";
+import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { EditDailyLogContent } from "./_components/edit-content";
-import { Spinner } from "@/components/ui/spinner";
+import { EditSkeleton } from "./_components/edit-skeleton";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "@/components/shared/error-boundary-fallback";
-import { notFound } from "next/navigation";
-import { TRPCError } from "@trpc/server";
 
 type PageProps = {
   params: Promise<{ id: string }>;
@@ -16,27 +14,11 @@ const EditDailyLogPage = async ({ params }: PageProps) => {
   const { id } = await params;
   const queryClient = getQueryClient();
 
-  try {
-    const log = await caller.daily_log.getById({ id });
-    queryClient.setQueryData(
-      trpc.daily_log.getById.queryOptions({ id }).queryKey,
-      JSON.parse(JSON.stringify(log)),
-    );
-  } catch (error) {
-    if (error instanceof TRPCError && error.code === "NOT_FOUND") {
-      notFound();
-    }
-    throw error;
-  }
-
+  void queryClient.prefetchQuery(trpc.daily_log.getById.queryOptions({ id }));
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <Suspense
-        fallback={
-          <div className="flex items-center justify-center min-h-screen">
-            <Spinner className="h-8 w-8" />
-          </div>
-        }
+        fallback={<EditSkeleton />}
       >
         <ErrorBoundary FallbackComponent={ErrorFallback}>
           <EditDailyLogContent id={id} />
